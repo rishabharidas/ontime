0552c0182aa5a41dfc9429e37300213fc49fd6cf
Rishab Haridas <rishabharidas010@gmail.com>
2025-03-11 06:12:53 +0000

    ad initial commit for electorn app

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# ontime
+## onTime Electron app
+# app to track time
+
+```
+npm run start
+```
+
+work in progress
---
+++ index.js
@@ -0,0 +1,17 @@
+const { app, BrowserWindow } = require("electron");
+
+const appWindow = () => {
+  const instance = new BrowserWindow({
+    width: 600,
+    height: 400,
+  });
+  instance.loadFile("index.html");
+};
+
+app.whenReady().then(() => {
+  appWindow();
+  console.log("testing election here ... . . . . .");
+  app.on("activate", () => {
+    if (!BrowserWindow.getAllWindows().length) appWindow();
+  });
+});
